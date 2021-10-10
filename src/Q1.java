f7060bd28b2e78a372c15ed1e985edcc22a08490
Himanshu Kushwaha <kushwaha0312@gmail.com>
2021-10-10 12:55:54 +0530

    Write a program to check the input number is greater then 100 and less then 500 is even or odd?

---
+++ src/Q1.java
@@ -0,0 +1,23 @@
+/*
+Q1. Write a program to check the input number is greater then 100 and less then 500 is even or odd?
+ */
+
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        if(n>100 && n<500)
+        {
+            if(n%2 == 0)
+                System.out.println("Even");
+            else
+                System.out.println("Odd");
+        }
+        else
+            System.out.println("Invalid input");
+    }
+}
