aea3330e2afdf0230e655a1ac1f6ddcb48bca13f
Himanshu Kushwaha <kushwaha0312@gmail.com>
2021-10-12 01:41:03 +0530

    Write a program to Calculate the factorial of given number.

---
+++ src/Assignment_2/Q3.java
@@ -0,0 +1,27 @@
+package Assignment_2;
+/*
+Ques3. Write a program to Calculate the factorial of given number.
+
+ */
+import java.util.Scanner;
+
+public class Q3 {
+    int facto(int n)
+    {
+        int f=1;
+        for(int i=1;i<=n;i++)
+        {
+            f=f*i;
+        }
+        return f;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        Q3 obj=new Q3();
+        System.out.println("ENTER THE NUMBER");
+        int N=sc.nextInt();
+        System.out.println(obj.facto(N));
+    }
+}
